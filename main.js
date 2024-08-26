70db8d658fc29fb10abb3f1a474e3dd9a0420686
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-08-26 16:26:11 +0300

    EDT-414/Feat:shortened the code

--- main.js
+++ main.js
@@ -3,50 +3,33 @@ let intervalId;
 document.querySelectorAll(".dropdown-toggle").forEach((e) => {
   e.addEventListener("click", (e) => {
     const menu = e.currentTarget.dataset.path;
+    const dataTargetMenu = document.querySelector(`[data-target=${menu}]`);
     document.querySelectorAll(".dropdown-menu").forEach((e) => {
-      if (
-        !document
-          .querySelector(`[data-target=${menu}]`)
-          .classList.contains("open")
+      if (!dataTargetMenu.classList.contains("open")
       ) {
-        e.classList.remove("menu-active");
-        e.classList.remove("open");
-        document
-          .querySelector(`[data-target=${menu}]`)
-          .classList.add("menu-active");
+        e.classList.remove("menu-active","open");
+        dataTargetMenu.classList.add("menu-active");
         intervalId = setTimeout(() => {
-          document.querySelector(`[data-target=${menu}]`).classList.add("open");
+            dataTargetMenu.classList.add("open");
         }, 0);
       }
       if (
-        document
-          .querySelector(`[data-target=${menu}]`)
-          .classList.contains("open")
+        dataTargetMenu.classList.contains("open")
       ) {
         clearTimeout(intervalId);
-        document
-          .querySelector(`[data-target=${menu}]`)
-          .classList.remove("menu-active");
+        dataTargetMenu.classList.remove("menu-active");
         intervalId = setTimeout(() => {
-          document
-            .querySelector(`[data-target=${menu}]`)
-            .classList.remove("open");
+            dataTargetMenu.classList.remove("open");
         }, 0);
       }
 
       window.onclick = (e) => {
         if (
-          e.target == document.querySelector(`[data-target=${menu}]`) ||
-          e.target == document.querySelector(`[data-path=${menu}]`)
+          e.target == dataTargetMenu || e.target == document.querySelector(`[data-path=${menu}]`)
         ) {
           return;
         } else {
-          document
-            .querySelector(`[data-target=${menu}]`)
-            .classList.remove("menu-active");
-          document
-            .querySelector(`[data-target=${menu}]`)
-            .classList.remove("open");
+        dataTargetMenu.classList.remove("menu-active", "open");
         }
       };
     });
